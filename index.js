0aa79fd81143410630cc0413d1bbae4d55c10b86
raghavendra1207 <112899843+raghavendra1207@users.noreply.github.com>
2022-09-18 20:49:17 +0530

    addover

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const a="Hello Node.js!";
-console.log(a);
+
+const fs = require("fs");
+const http=require("http");
+
+const server=http.createServer((req,res)=>
+{
+fs.readFile("sample.txt",(err,data)=>
+{
+res.end(data);
+})
+});
+server.listen(3000);
+/*console.log(a);
+const fs=require("fs");
+fs.writeFile("sample.txt","hello world welcome to node js",(err) =>
+{
+    if(err) throw err;
+    console.log("created");
+});
+
+fs.readFile("sample.txt",(err,data)=>
+{
+    if(err) throw err;
+    console.log(data.toString());
+});*/
